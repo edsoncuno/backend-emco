48bf84e88509dd69bcc3b4205906497a270e2add
= <=>
2022-05-16 15:03:08 -0500

    instale mongoose y la conexion es exitosa

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,5 @@ dotenv.config();
 
 export default {
     SERVER_PORT: process.env.SERVER_PORT,
-    MONGODB_HOST: process.env.MONGODB_HOST,
-    MONGODB_NAME: process.env.MONGODB_NAME,
-    MONGODB_USER: process.env.MONGODB_USER,
-    MONGODB_PASSWORD: process.env.MONGODB_PASSWORD
+    MONGOOSE_URI: process.env.MONGOOSE_URI,
 }
--- src/views/App.ts
+++ src/views/App.ts
@@ -1,10 +1,10 @@
-import "reflect-metadata"
 import config from '../config';
 import express from 'express'
 import cors from 'cors';
 import morgan from 'morgan';
 import routers from './routers';
 import errorHandle from './middlewares/error.middleware';
+import { connect } from 'mongoose';
 
 export default class App {
     private app: express.Application;
@@ -21,10 +21,20 @@ export default class App {
         this.app.use(errorHandle);
     }
 
-    public run() {
+    private async connectToMongoDB(): Promise<void> {
+        try {
+            const database = await connect(String(config.MONGOOSE_URI));
+            console.log('Connected to the database: ', database.connection.name);
+        } catch (error: any) {
+            console.error(error.name);
+            console.error(error.message);
+        }
+    }
+
+    public async run() {
+        await this.connectToMongoDB();
         this.app.listen(config.SERVER_PORT, () => {
             console.log(`Connected successfully to server on port: ${config.SERVER_PORT} - http://localhost:${config.SERVER_PORT}`);
         });
     }
-
 }
